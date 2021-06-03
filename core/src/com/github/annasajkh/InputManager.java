14e3ec5e6fb099553b02702d646bdacc23e9aaac
Annas <61279556+annasajkh@users.noreply.github.com>
2021-06-03 22:39:56 +0700

    Update InputManager.java

--- core/src/com/github/annasajkh/InputManager.java
+++ core/src/com/github/annasajkh/InputManager.java
@@ -46,7 +46,6 @@ public class InputManager implements InputProcessor
 	@Override
 	public boolean touchUp(int screenX, int screenY, int pointer, int button)
 	{
-		// TODO Auto-generated method stub
 		return false;
 	}
 
